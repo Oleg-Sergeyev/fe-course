c0e5f654b2e2478dc2d9d72eb0e26ef9b0d0cf0b
Oleg Sergeyev <sergeyev_ov@mail.ru>
2022-07-18 18:35:33 +0300

    Добавление рейтинга, начало

---
+++ app/javascript/components/Comments/Likes.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
+
+const Likes = () => {
+    const [count, setCount] = React.useState(0);
+ 
+    return (
+      <div className="container">
+      
+      </div>
+    );
+  };
+  export default Likes;
