658d7cc6e3258293aa3eab90ba4b8c36d034e2a0
Oleg Sergeyev <sergeyev_ov@mail.ru>
2022-07-25 01:21:54 +0300

    Добавлен useQuery для вывода всех новостей

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,19 +4,24 @@ import NewAuthForm from './Auth/NewAuthForm'
 import News from './News/news';
 import AllNews from './News/allnews';
 import TopMenu from './TopMenu/MainMenu';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 localStorage.setItem("token", document.querySelector('meta[name="csrf-token"]').content);
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <TopMenu />
-        <Routes>
-          <Route path="/" element={<AllNews />} />
-          <Route path="/news" element={<AllNews />} />
-          <Route path="/news/:id" element={<News />} />
-          <Route path="/users/sign_in" element={<NewAuthForm />} />
-        </Routes>
-    </BrowserRouter>
+  return (<QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <TopMenu />
+                <Routes>
+                  <Route path="/" element={<AllNews />} />
+                  <Route path="/news" element={<AllNews />} />
+                  <Route path="/news/:id" element={<News />} />
+                  <Route path="/users/sign_in" element={<NewAuthForm />} />
+                </Routes>
+            </BrowserRouter>
+          </QueryClientProvider>
   );
 }
 export default App;
--- app/javascript/components/News/allnews.jsx
+++ app/javascript/components/News/allnews.jsx
@@ -1,35 +1,40 @@
 import * as React from 'react';
 import { Table } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import Likes from "../Comments/Likes";
+import { useQuery } from '@tanstack/react-query';
 
 const AllNews = () => {
   const [array, setArray] = React.useState([]);
-    React.useEffect(() => {
-      fetch(`/news.json`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        setArray(data);
-        localStorage.setItem("AllNews", JSON.stringify(data));
-        console.log('All news:', data);
-      })
-      .catch((error) => {
+
+const { isLoading, data, error } = useQuery([],() =>
+  fetch(`/news.json`,{
+  method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => response.json())
+)
+
+  React.useEffect(() => {
+    if (isLoading) {
+      return
+    }
+    if (error) {
       console.error('Error:', error);
-      });
-  }, []);
+      return;
+    }
+      setArray(data);
+      localStorage.setItem("AllNews", JSON.stringify(data));
+  },[isLoading, data, error])
 
   const renderTR = () => {
-    let td: any[] = [];
+    let td = [];
     for (let i = array.length - 1; i >= 0; i -= 1) {
       td.push(<tr key={i}>
                 <td><span className='news-tr-td'><Link to={`/news/${array[i]["id"]}`}>{array[i]["date"]}</Link></span></td>
                 <td><span className='news-tr-td'><Link to={`/news/${array[i]["id"]}`}>{array[i]["header"]}</Link></span></td>
-                {/* <td><span className='news-tr-td'><Likes likes={array[i]["simple_rating"]} heart_visible={false}/></span></td> */}
                 <td><span className='news-tr-td'><Link to={`/news/${array[i]["id"]}`}>{array[i]["simple_rating"]}</Link></span></td>
               </tr>);
     }
